05ea812386d7c900be0a50758d750135c34b02d1
ak4m410x01 <abdallah10kamal@gmail.com>
2023-08-11 22:25:58 +0300

    link client side with server side variables

--- job/urls.py
+++ job/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "jobs"
+
 urlpatterns = [
-    path("", views.jobs),
-    path("<int:id>", views.job_details),
+    path("", views.jobs, name="jobs_list"),
+    path("<int:id>", views.job_details, name="job_details"),
 ]
